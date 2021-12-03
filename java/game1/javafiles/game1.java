f8a22219d03ee2ca6dc0e5a254ae7ed502f4ad60
Barrett Wise <barrettkwise@gmail.com>
2021-12-03 12:59:29 -0500

    no longer making gui

--- java/game1/javafiles/game1.java
+++ java/game1/javafiles/game1.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class game1 {
-    public static void main(String args[]) throws IOException {    
+    public static void main(String args[]) throws IOException {
         //intro to game
         Scanner in = new Scanner (System.in);
         System.out.println("Welcome to Word Jeopardy!");
