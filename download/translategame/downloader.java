524b3bcc8a669ecf880c6916870f68464be58464
Barrett Wise <barrettkwise@gmail.com>
2021-12-13 13:57:17 -0500

    added py installer and made file path changable

---
+++ download/translategame/downloader.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+//downloads python packages
+public class downloader {
+    public static void downloader () throws IOException {
+        File file = new File("status.txt");
+        try {
+            String read = "";
+            Scanner in = new Scanner(file);
+            while (in.hasNextLine()) {
+                read = in.nextLine();
+                if (read.equalsIgnoreCase("yes")) {
+                    String app = "notepad.exe";
+                    String cmd = "status.txt";
+                    ProcessBuilder pb = new ProcessBuilder(app, cmd);
+                    pb.start();
+                    System.out.println("Module already installed");
+                }
+            }
+            in.close();
+        }
+        
+        catch(IOException e) {
+            System.out.println("File not found, starting process.");
+            String app = "py";
+            String cmd = "-m pip install requests";
+            ProcessBuilder pb = new ProcessBuilder(app, cmd);
+            pb.start();
+            file.createNewFile();
+            FileWriter filewrite = new FileWriter(file);
+            BufferedWriter write = new BufferedWriter(filewrite);
+            write.write("yes");
+            write.close();
+            System.out.println("Module installed");
+        }
+    }
+}
+
---
+++ download/translategame/gameselect.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+//makes it easier for user to
+//select game/start python
+public class gameselect {
+    public static void main(String args[]) throws IOException {
+        //select game
+        try {
+            Scanner in = new Scanner(System.in);
+            downloader d = new downloader();
+            d.downloader();
+            System.out.println("Enter the name assigned to your Windows user: ");
+            String name = in.nextLine();
+            //start python
+            String app = "py";
+            String path = "C:/Users/" + name + "/Downloads/translategame-master/download/ngramcount.py";
+            ProcessBuilder pb = new ProcessBuilder(app, path);
+            Process p = pb.start();
+            game1 g = new game1();
+            game2 f = new game2();
+            int choice = 0;
+            System.out.println("Welcome to TranslateGame!");
+            System.out.println("Type 1 for game1 (words) or 2 for game2 (phrases)");
+            while (choice != 1 && choice != 2) {
+                choice = in.nextInt();
+                if (choice != 1 && choice != 2) {
+                    System.out.println("No game associated with that number.");
+                }
+            }
+            if (choice == 1) {
+                g.game1();
+            }
+            else if (choice == 2) {
+                f.game2();
+            }
+        }
+        catch(IOException e) {
+            System.out.println("No.");
+        }
+    }
+}
